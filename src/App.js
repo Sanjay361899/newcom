535764bc33c47b50b22791fbc25459f76b8c3599
sanjay thakur <sanjaythakur361899@gmail.com>
2024-01-09 10:18:59 +0530

    scss new styles addition

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './styles/app.scss'
 import { Suspense, lazy } from 'react';
 import { loading } from './components/loading.tsx';
 import Header from './components/header.tsx';
+const Dashboard=lazy(()=>import('./pages/admin/dashboard.tsx'));
+const Customer=lazy(()=>import('./pages/admin/customer.tsx'));
+const Transactions=lazy(()=>import('./pages/admin/transactions.tsx'));
+const Product=lazy(()=>import('./pages/admin/product.tsx'))
 const Home =lazy(()=>import('./pages/home.tsx'));
 const Search =lazy(()=>import('./pages/search.tsx'));
 const Cart =lazy(()=>import('./pages/cart.tsx'));
@@ -15,6 +19,11 @@ function App() {
   <Suspense fallback={loading}>
   <Routes>
     <Route path='/' element={<Home/>}/> 
+    {/* admin */}
+    <Route path='/admin/dashboard' element={<Dashboard/>}/> 
+    <Route path='/admin/product' element={<Product/>}/> 
+    <Route path='/admin/transaction' element={<Transactions/>}/> 
+    <Route path='/admin/customer' element={<Customer/>}/> 
     <Route path='/Search' element={<Search/>}/> 
     <Route path='/Cart' element={<Cart/>}/> 
   </Routes>
